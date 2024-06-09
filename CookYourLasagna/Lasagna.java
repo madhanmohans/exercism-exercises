0f32a3a1b5f7ab43b174a37d3fbb45f9dbcdcc1d
Madhan Mohan S <madhanmohans@Madhans-MacBook-Pro.local>
2024-06-09 11:59:35 +0530

    Add. final variable used

--- CookYourLasagna/Lasagna.java
+++ CookYourLasagna/Lasagna.java
@@ -1,14 +1,17 @@
 package CookYourLasagna;
 
 public class Lasagna {
+
+    private final int expectedMinutesInOven = 40;
+
     private int expectedMinutesInOven() {
-        return 40;
+        return expectedMinutesInOven;
     }
     public int remainingMinutesInOven(int minutesInOven) {
         return expectedMinutesInOven() - minutesInOven;
     }
     private int preparationTimeInMinutes(int numberOfLayers) {
-        return 2*numberOfLayers;
+        return 2 * numberOfLayers;
     }
     
     public int totalTimeInMinutes(int numberOfLayers, int minutesInOven) {
